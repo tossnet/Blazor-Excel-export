dee53bd8a8fe4fd49a751f55a30d301d077f409a
tossnet <tossnet@users.noreply.github.com>
2022-10-22 08:38:09 +0000

    deploy: 82b4994fc37743e2c9644fb7506244c5b4ce70c4

---
+++ js/openfile.js
@@ -0,0 +1,30 @@
+﻿function BlazorDownloadFile(filename, content) {
+    // thanks to Geral Barre : https://www.meziantou.net/generating-and-downloading-a-file-in-a-blazor-webassembly-application.htm 
+
+    // Create the URL
+    const file = new File([content], filename, { type: "application/octet-stream" });
+    const exportUrl = URL.createObjectURL(file);
+
+    // Create the <a> element and click on it
+    const a = document.createElement("a");
+    document.body.appendChild(a);
+    a.href = exportUrl;
+    a.download = filename;
+    a.target = "_self";
+    a.click();
+
+    // We don't need to keep the object url, let's release the memory
+    // On Safari it seems you need to comment this line... (please let me know if you know why)
+    URL.revokeObjectURL(exportUrl);
+}
+
+function BlazorOpenFile(filename, content) {
+
+    // Create the <a> element and click on it
+    const a = document.createElement("a");
+    document.body.appendChild(a);
+    a.href = filename;
+    a.download = filename;
+    a.target = "_self";
+    a.click();
+}
